841a9ac99106a7c479502f53de349acb888b1cd6
Polycarp Kavoo Kyalo <pkyalo@safaricom.co.ke>
2022-05-27 13:04:45 +0300

    decrypt test

--- src/test/java/DecryptTest.java
+++ src/test/java/DecryptTest.java
@@ -1,2 +1,29 @@
-public class DecryptTest {
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class EncryptTest {
+
+    @Test
+    public void runEncrypt_instantiates(){
+        Encrypt testEncrypt = new Encrypt("abc",2);
+        assertEquals(true, testEncrypt instanceof Encrypt);
+    }
+
+    @Test
+    public void runEncrypt_ifInputIsString() {
+        Encrypt testEncrypt = new Encrypt("abc",3);
+        assertEquals("abc", testEncrypt.getmInputString());
+    }
+
+    @Test
+    public void runDecrypt_ifKeyIsInt() {
+        Encrypt testEncrypt = new Encrypt("abc",2);
+        assertEquals(2, testEncrypt.getmShift());
+    }
+    @Test
+    public void runEncrypt_ifEncryptsVariousInputs() {
+        Encrypt testEncrypt = new Encrypt("!LAZY BROWN FOX JUMPS OVER A LAZY DOG2", 3);
+        // does not decrypt integers and other characters but returns them as is
+        assertEquals("!ODCB EURZQ IRA MXPSV RYHU D ODCB GRJ2",Encrypt.encrypt(testEncrypt));
+    }
 }
