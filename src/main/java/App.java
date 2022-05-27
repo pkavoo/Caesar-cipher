48b2196ff3c528cddf995e4661e717087d1137a8
Polycarp Kavoo Kyalo <pkyalo@safaricom.co.ke>
2022-05-27 12:22:33 +0300

    Encrypt and decrypt

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,51 @@
+import java.io.Console;
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        boolean runCipher = true;
+
+        while (runCipher) {
+
+            System.out.println("Welcome to my Caesar Cipher");
+            System.out.println("============================");
+            System.out.println("Enter encrypt to encrypt or decrypt to decrypt or exit to leave the app");
+            String choice = myScanner.nextLine();
+
+            if (choice.equals("encrypt")) {
+                System.out.println("Encrypt your message by entering it in the next line");
+                String input = myScanner.nextLine();
+                System.out.println("============================");
+
+                System.out.println("Enter a key");
+                int key = myScanner.nextInt();
+                System.out.println("============================");
+                Encrypt cipher = new Encrypt(input,key);
+                String encrypted = Encrypt.encrypt(cipher);
+
+                System.out.println("Encrypted message: " + encrypted);
+                System.out.println("============================");
+                System.out.println("============================");
+                System.out.println("============================");
+            } else if (choice.equals("decrypt")) {
+                System.out.println("To decrypt a message enter it below");
+                String sema = myScanner.nextLine();
+                System.out.println("============================");
+
+                System.out.println("Enter the key you used");
+                int namba = myScanner.nextInt();
+                System.out.println("============================");
+                Decrypt ceaser = new Decrypt(sema,namba);
+                String decrypted = Decrypt.decrypt(ceaser);
+
+                System.out.println("Decrypted message: " + decrypted);
+                System.out.println("============================");
+                System.out.println("============================");
+                System.out.println("============================");
+            } else if (choice.equals("exit")) {
+                runCipher = false;
+            }
+        }
+    }
 }
--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -1,2 +1,57 @@
 public class Encrypt {
+    private String mInputString;
+    private int mShift;
+
+    public String getmInputString(){
+        return mInputString;
+    }
+
+    public int getmShift(){
+        return mShift;
+    }
+
+    public Encrypt(String mInputString, int mShift) {
+        this.mInputString = mInputString;
+        this.mShift = mShift;
+    }
+
+    public static String encrypt(Encrypt encrypt){
+
+        if(encrypt.mShift > 26){
+            encrypt.mShift = encrypt.mShift % 26;
+        }
+        else if(encrypt.mShift < 0) {
+            encrypt.mShift = (encrypt.mShift % 26) + 26;
+        }
+
+        String outputString = "";
+        int length = encrypt.mInputString.length();
+        for (int i = 0; i < length ; i++) {
+            char cc = encrypt.mInputString.charAt(i);
+            if(Character.isLetter(cc)){
+                if(Character.isLowerCase(cc)){
+                    char c = (char)(cc+encrypt.mShift);
+                    if(c>'z'){
+                        outputString += (char)(cc - (26-encrypt.mShift));
+                    }
+                    else {
+                        outputString += c;
+                    }
+                }
+                else if(Character.isUpperCase(cc)){
+                    char c = (char)(cc+encrypt.mShift);
+                    if(c>'Z'){
+                        outputString += (char)(cc - (26-encrypt.mShift));
+                    }
+                    else {
+                        outputString += c;
+                    }
+                }
+            }
+            else{
+                outputString += cc;
+            }
+        }
+        return outputString;
+    }
 }
